e187ce689f8ac9a546f4f6832c8c92bb40e35978
Eike Lurz <eike.lurz@tuta.io>
2022-12-02 22:47:35 +0100

    Add day02 part 2

--- 2022/day02/main.go
+++ 2022/day02/main.go
@@ -14,15 +14,32 @@ const (
 	Scissor
 )
 
+func (this Move) String() string {
+	switch this {
+	case Rock:
+		return "Rock"
+	case Paper:
+		return "Paper"
+	default: /* Scissor */
+		return "Scissor"
+	}
+}
+
 func (this Move) fight(other Move) int {
+	var res int
 	if this == other {
-		return 3 + (int(this + 1))
-	}
-	if (int(this)+1)%3 == int(other) {
-		return 0 + int(this+1)
+		res = 3 + this.value()
+	} else if (int(this)+1)%3 == int(other) {
+		res = 0 + this.value()
 	} else {
-		return 6 + int(this+1)
+		res = 6 + this.value()
 	}
+
+	return res
+}
+
+func (this Move) value() int {
+	return int(this) + 1
 }
 
 type Round struct {
@@ -40,8 +57,7 @@ func strToMove(m string) Move {
 		return Rock
 	case "B", "Y":
 		return Paper
-	default:
-		// "C", "Z"
+	default: /* "C", "Z" */
 		return Scissor
 	}
 }
@@ -72,6 +88,70 @@ func part1() int {
 	return play(lines)
 }
 
+type Result int
+
+const (
+	Lose Result = iota
+	Draw
+	Win
+)
+
+func (this Result) String() string {
+	switch this {
+	case Lose:
+		return "Lose"
+	case Draw:
+		return "Draw"
+	default: /* Win */
+		return "Win"
+	}
+}
+
+func strToResult(s string) Result {
+	switch s {
+	case "X":
+		return Lose
+	case "Y":
+		return Draw
+	default: /* Z */
+		return Win
+	}
+}
+
+func (this Move) predict(result Result) int {
+	var res int
+	if result == Draw {
+		res = 3 + this.value()
+	} else if result == Win {
+		res = 6 + Move((int(this)+1)%3).value()
+	} else {
+		res = 0 + Move((int(this)+2)%3).value()
+	}
+
+	return res
+}
+
+func predict(lines []string) int {
+	var sum int
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+		parts := strings.Split(line, " ")
+		move := strToMove(parts[0])
+		res := strToResult(parts[1])
+		sum += move.predict(res)
+	}
+
+	return sum
+}
+
+func part2() int {
+	lines := helpers.ReadLines("input")
+	return predict(lines)
+}
+
 func main() {
-	fmt.Println(part1())
+	// fmt.Println(part1())
+	fmt.Println(part2())
 }
