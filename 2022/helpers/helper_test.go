11cf808c52506034ce66267ef316196ba6959435
Eike Lurz <eike.lurz@tuta.io>
2022-12-05 22:57:57 +0100

    Add helpers

---
+++ 2022/helpers/helper_test.go
@@ -0,0 +1,31 @@
+package helpers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFold(t *testing.T) {
+	sum := Foldr(0, []int{1, 2, 3, 4, 5}, func(i int, a int, e int) int {
+		return a + e
+	})
+
+	assert.Equal(t, 15, sum, "Sum after foldr should be 15")
+}
+
+func TestMap(t *testing.T) {
+	res := Map([]int{1, 2, 3, 4, 5}, func(i int, e int) int {
+		return e + 1
+	})
+
+	assert.Equal(t, []int{2, 3, 4, 5, 6}, res, "Element should be incremented by 1 after mapping")
+}
+
+func TestFilter(t *testing.T) {
+	res := Filter([]int{1, 2, 3, 4, 5, 6}, func(i int, e int) bool {
+		return (e%2 == 0)
+	})
+
+	assert.Equal(t, []int{2, 4, 6}, res, "Filter should filter out odd numbers")
+}
--- 2022/helpers/helpers.go
+++ 2022/helpers/helpers.go
@@ -2,6 +2,7 @@ package helpers
 
 import (
 	"log"
+	"math"
 	"os"
 	"strings"
 )
@@ -21,3 +22,53 @@ func ReadLines(input string) []string {
 	lines := strings.Split(content, "\n")
 	return lines[:len(lines)-1] // drop empty last line
 }
+
+func Foldr[I any, O any](a O, l []I, fn func(int, O, I) O) O {
+	for i, e := range l {
+		a = fn(i, a, e)
+	}
+
+	return a
+}
+
+func Map[I any, O any](l []I, fn func(int, I) O) []O {
+	return Foldr(make([]O, 0, len(l)), l, func(i int, a []O, l I) []O {
+		a = append(a, fn(i, l))
+		return a
+	})
+}
+
+func Filter[I any](l []I, p func(int, I) bool) []I {
+	return Foldr(make([]I, 0, len(l)), l, func(i int, a []I, l I) []I {
+		if p(i, l) {
+			a = append(a, l)
+		}
+		return a
+	})
+}
+
+func TakeWhile[I any](l []I, p func(i int, e I) bool) []I {
+	res := make([]I, 0, cap(l))
+	for i, e := range l {
+		if !p(i, e) {
+			break
+		}
+
+		res = append(res, e)
+	}
+
+	return res
+}
+
+func Take[I any](l []I, n int) []I {
+	res := make([]I, 0, n)
+	for i := 0; i < int(math.Max(float64(n), float64(len(l)))); i++ {
+		res = append(res, l[i])
+	}
+
+	return res
+}
+
+// func Drop[I any](l []I, n int) []I {
+
+// }
