73a27874c1a80c6946b39431c092532e9ba04ada
Eike Lurz <eike.lurz@tuta.io>
2023-08-06 16:02:23 +0200

    Day 03

--- 2015/bin/main.ml
+++ 2015/bin/main.ml
@@ -1,20 +1,39 @@
 open Aoc2015
 
-let () =
+let d1 =
   print_endline "## Day 01 ##";
-  let d01_input = Input.read_all "input/d01" in
-  let floor = D01.move d01_input in
+  let input = Input.read_all "input/d01" in
+  let floor = D01.move input in
   Int.to_string floor |> print_endline;
 
-  let position = D01.detect_cellar d01_input in
+  let position = D01.detect_cellar input in
   Int.to_string position |> print_endline;
+  ()
 
+let d2 =
   print_endline "## Day 02 ##";
-  let d02_input = Input.read_lines "input/d02" in
-  let paper = D02.determine_paper d02_input in
+  let input = Input.read_lines "input/d02" in
+  let paper = D02.determine_paper input in
   Int.to_string paper |> print_endline;
 
-  let ribbon = D02.determine_ribbon d02_input in
+  let ribbon = D02.determine_ribbon input in
   Int.to_string ribbon |> print_endline;
+  ()
+
+let d3 =
+  print_endline "## Day 03 ##";
+  let input = Input.read_all "input/d03" in
+  let houses = D03.deliver_packages input in
+  Int.to_string houses |> print_endline;
+
+  let other_houses = D03.deliver_packages_with_robo input in
+  Int.to_string other_houses |> print_endline;
+
+  ()
+
+let () =
+  d1;
+  d2;
+  d3;
 
   ()
