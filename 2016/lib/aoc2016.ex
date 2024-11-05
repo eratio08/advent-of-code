dfa8dd71ebbe8142a91b52565290910e11171822
Eike Lurz <eike.lurz@tuta.io>
2024-11-05 21:08:49 +0100

    Ininit aoc 2016

---
+++ 2016/lib/aoc2016.ex
@@ -0,0 +1,18 @@
+defmodule Aoc2016 do
+  @moduledoc """
+  Documentation for `Aoc2016`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Aoc2016.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ 2016/test/aoc2016_test.exs
@@ -0,0 +1,8 @@
+defmodule Aoc2016Test do
+  use ExUnit.Case
+  doctest Aoc2016
+
+  test "greets the world" do
+    assert Aoc2016.hello() == :world
+  end
+end
