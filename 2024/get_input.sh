179b371aefac47d01c420697d03930c870d7b288
Eike Lurz <eike.lurz@tuta.io>
2024-12-01 12:57:49 +0100

    🏂 Adjust the get_input helper to properly use session cookie

--- 2024/get_input.sh
+++ 2024/get_input.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -b session="$2" https://adventofcode.com/2024/day/"$1"/input -o "input/d$1"
+curl --cookie "session=$2" https://adventofcode.com/2024/day/"$1"/input -o "input/test-d$1"
